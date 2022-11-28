6cfb5157b8fbc896ff82835a11228dafcd6c6642
Akabhishek067 <akabhishek067@gmail.com>
2022-11-28 12:24:08 +0530

    added new classes comp & state change example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './Product';
 
 function App() {
+  const myName = 'abhishek';
+  const myCondition = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>inside app </h2>
+      <Product />
     </div>
   );
 }
